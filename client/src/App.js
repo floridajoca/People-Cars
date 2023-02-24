bcf9cd2d5b933da8ac06b1de2813bb323b1da240
floridajoca <florida.joca@gmail.com>
2023-02-24 12:48:10 -0600

    Install React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import {ApolloProvider, ApolloClient, InMemoryCache} from "@apollo/client";
+import {Layout} from "antd";
+import {Header} from "antd/es/layout/layout";
 import AddCar from "./components/forms/AddCar";
 import AddPerson from "./components/forms/AddPerson";
 import Cars from "./components/list/Cars";
 import People from "./components/list/People";
 import Title from "./components/Title";
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import 'antd/dist/reset.css'
+import Home from "./Home";
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000/graphql',
@@ -16,13 +20,17 @@ const client = new ApolloClient({
 const App = () => {
     return (
         <ApolloProvider client={client}>
-            <div className="App">
-              <Title/>
-                <AddPerson/>
-                <AddCar/>
-                <h3>Records</h3>
-                <People/>
-            </div>
+
+
+            <BrowserRouter>
+                <Layout className="App">
+
+                    <Routes>
+                        <Route path="/" element={<Home />}></Route>
+                        {/*<Route path="person/:personId" element={<SinglePage />} />*/}
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
         </ApolloProvider>
   );
 }
